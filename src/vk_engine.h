25962a78aed3eaab1ad7c149ded8ecd65d6cd9b9
Dusan Fajler <dusanfajler@gmail.com>
2024-10-07 23:52:50 +0200

    vk guide chapter 1 finished

---
+++ src/vk_engine.h
@@ -0,0 +1,65 @@
+﻿// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <vk_types.h>
+
+constexpr unsigned int FRAME_OVERLAP = 2; // Double (triple) buffer
+
+struct FrameData {
+
+	VkCommandPool _commandPool;
+	VkCommandBuffer _mainCommandBuffer;
+	VkSemaphore _swapchainSemaphore, _renderSemaphore;
+	VkFence _renderFence;
+};
+
+
+class VulkanEngine
+{
+public:
+	VkInstance _instance;
+	VkDebugUtilsMessengerEXT _debug_messenger;
+	VkPhysicalDevice _chosenGPU;
+	VkDevice _device;
+	VkSurfaceKHR _surface;
+
+	bool _isInitialized{ false };
+	int _frameNumber {0};
+	bool stop_rendering{ false };
+	VkExtent2D _windowExtent{ 1700 , 900 };
+
+	struct SDL_Window* _window{ nullptr };
+
+	static VulkanEngine& Get();
+
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
+
+	FrameData _frames[FRAME_OVERLAP];
+
+	FrameData& get_current_frame() { return _frames[_frameNumber % FRAME_OVERLAP]; };
+
+	VkQueue _graphicsQueue;
+	uint32_t _graphicsQueueFamily;
+
+
+	void init();
+	void cleanup();
+	void draw();
+	void run();
+
+private:
+	void initVulkan();
+	void initSwapchain();
+	void initCommands();
+	void initSyncStructures();
+
+	void createSwapchain(uint32_t width, uint32_t height);
+	void destroySwapchain();
+};
---
+++ src/vk_images.h
@@ -0,0 +1,8 @@
+
+#pragma once 
+
+namespace vkutil {
+
+	void transition_image(VkCommandBuffer cmd, VkImage image, VkImageLayout currentLayout, VkImageLayout newLayout);
+
+};
